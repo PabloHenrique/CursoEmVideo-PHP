c59db89ee35cedbf563b08fe9d93f2c08dcb6b1f
PabloHenrique <pablohenriquenadai@gmail.com>
2022-02-13 14:27:45 -0300

    2022 02 13 - Order Arquivos

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Index.php</title>
+    </head>
+    <body>
+        <h2>Arquivos PHP</h2>
+        <ol>
+            <li><a href="./olamundo.php">Olá Mundo!</a></li>
+        </ol>
+    </body>
+</html>
