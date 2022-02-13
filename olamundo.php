cfb5700a3c665a1b16cab564b1e3578f62bb5449
PabloHenrique <pablohenriquenadai@gmail.com>
2022-02-13 13:54:56 -0300

    first commit

---
+++ olamundo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Olá Mundo</title>
+        <style>
+            h2{
+                color: #000;
+            }
+            h3{
+                color: gray;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Testando o PHP</h2>
+        <?php
+            echo "<h3>Olá Mundo!<h3>";
+        ?>
+    </body>
+</html>
