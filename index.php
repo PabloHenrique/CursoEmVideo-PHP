ad65cbd052fd65db8a2a300809041e05f7a417d7
PabloHenrique <pablohenriquenadai@gmail.com>
2022-02-16 16:32:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,26 +4,37 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/estilos/style.css">
         <title>Index.php</title>
     </head>
     <body>
-        <h2>Arquivos PHP</h2>
-        <ol>
-            <li><a href="./olamundo.php">Olá Mundo!</a></li>
-            <li><a href="./tiposVar.php">Tipos de variáveis.</a></li>
-            <li><a href="./operadores.php">Operadores Aritméticos.</a></li>
-            <li><a href="./operadoresRelacionais.php">Operadores Relacionais.</a></li>
-            <li><a href="./operadoresLogicos.php">Operadores Lógicos.</a></li>
-            <li><a href="./funcoesAritmeticas.php">Funções Aritméticas.</a></li>
-            <li><a href="./igualdade.php">Igualdade.</a></li>
-        </ol>
-        <h2>Exercícios</h2>
-        <ol>
-            <li><a href="./exe01.php">Exercício 01</a></li>
-            <li><a href="./exe02.php">Exercício 02</a></li>
-            <li><a href="./exe03.php">Exercício 03</a></li>
-            <li><a href="./exe04.php">Exercício 04</a></li>
-            <li><a href="./exe05.php">Exercício 05</a></li>
-        </ol>
+        <div class="conteudo">
+            <h2>Arquivos PHP</h2>
+            <table class="tabela">
+                <tr>
+                    <td class="item"><a href="./olamundo.php">Olá Mundo!</a></td>
+                    <td class="item"><a href="./tiposVar.php">Tipos de variáveis.</a></td>
+                    <td class="item"><a href="./operadores.php">Operadores Aritméticos.</a></td>
+                    <td class="item"><a href="./operadoresRelacionais.php">Operadores Relacionais.</a></td>
+                    <td class="item"><a href="./operadoresLogicos.php">Operadores Lógicos.</a></td>
+                </tr>
+                <tr>
+                    
+                    
+                    <td class="item"><a href="./funcoesAritmeticas.php">Funções Aritméticas.</a></td>
+                    <td class="item"><a href="./igualdade.php">Igualdade.</a></td>
+                </tr>
+            </table>
+            <h2>Exercícios</h2>
+            <table class="tabela">
+                <tr>
+                    <td class="item"><a href="./exe01.php">Exercício 01</a></td>
+                    <td class="item"><a href="./exe02.php">Exercício 02</a></td>
+                    <td class="item"><a href="./exe03.php">Exercício 03</a></td>
+                    <td class="item"><a href="./exe04.php">Exercício 04</a></td>
+                    <td class="item"><a href="./exe05.php">Exercício 05</a></td>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
