832bc4ff8961c598645710637ebbf31c3c5a5252
PabloHenrique <pablohenriquenadai@gmail.com>
2022-02-18 13:13:45 -0300

    Path Update

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
             <h2>Arquivos PHP</h2>
             <table class="tabela">
                 <tr>
-                    <td class="item"><a href="./olamundo.php">Olá Mundo!</a></td>
-                    <td class="item"><a href="./tiposVar.php">Tipos de variáveis.</a></td>
-                    <td class="item"><a href="./operadores.php">Operadores Aritméticos.</a></td>
-                    <td class="item"><a href="./operadoresRelacionais.php">Operadores Relacionais.</a></td>
-                    <td class="item"><a href="./operadoresLogicos.php">Operadores Lógicos.</a></td>
+                    <td class="item"><a href="../conceitos/olamundo.php">Olá Mundo!</a></td>
+                    <td class="item"><a href="../conceitos/tiposVar.php">Tipos de variáveis.</a></td>
+                    <td class="item"><a href="../conceitos/operadores.php">Operadores Aritméticos.</a></td>
+                    <td class="item"><a href="../conceitos/operadoresRelacionais.php">Operadores Relacionais.</a></td>
+                    <td class="item"><a href="../conceitos/operadoresLogicos.php">Operadores Lógicos.</a></td>
                 </tr>
                 <tr>
                     
@@ -28,11 +28,11 @@
             <h2>Exercícios</h2>
             <table class="tabela">
                 <tr>
-                    <td class="item"><a href="./exe01.php">Exercício 01</a></td>
-                    <td class="item"><a href="./exe02.php">Exercício 02</a></td>
-                    <td class="item"><a href="./exe03.php">Exercício 03</a></td>
-                    <td class="item"><a href="./exe04.php">Exercício 04</a></td>
-                    <td class="item"><a href="./exe05.php">Exercício 05</a></td>
+                    <td class="item"><a href="../exercicios/exe01.php">Exercício 01</a></td>
+                    <td class="item"><a href="../exercicios/exe02.php">Exercício 02</a></td>
+                    <td class="item"><a href="../exercicios/exe03.php">Exercício 03</a></td>
+                    <td class="item"><a href="../exercicios/exe04.php">Exercício 04</a></td>
+                    <td class="item"><a href="../exercicios/exe05.php">Exercício 05</a></td>
                 </tr>
             </table>
         </div>
